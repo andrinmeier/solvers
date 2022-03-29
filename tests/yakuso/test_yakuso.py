dea9e33414236261e420f161c36a0fc8ec4a7306
Andrin Meier <andrin.meier@hispeed.ch>
2022-03-29 13:05:49 +0200

    Add complete solution for Yakuso
    The solver now uses two separate lists: a board with 0=no value and 1=max value and a separate list for keeping the max value per row. This proved to be the most simple solution.
    
    We also add constraints for the sums so that we could *in theory* generate valid Yakusos ourselves.

--- tests/yakuso/test_yakuso.py
+++ tests/yakuso/test_yakuso.py
@@ -19,7 +19,7 @@ def test_preinitializes_board_correctly():
     cols = 6
     template = get_template()
     sum_cols = get_sum_columns()
-    solved = yakuso_solver.solve(template, sum_cols, rows, cols)
+    solved = yakuso_solver.solve(template, sum_cols, rows, cols)[0]
     for i in range(rows):
         for j in range(cols):
             if len(template[i][j]) > 0:
@@ -31,10 +31,22 @@ def test_cols_sum_correctly():
     cols = 6
     template = get_template()
     sum_cols = get_sum_columns()
-    solved = yakuso_solver.solve(template, sum_cols, rows, cols)
+    solved = yakuso_solver.solve(template, sum_cols, rows, cols)[0]
     for j in range(cols):
         total = 0
         for i in range(rows):
             total += solved[i][j]
         if len(sum_cols[j]) > 0:
             assert total == int(sum_cols[j])
+
+def test_all_numbers_in_row_must_be_zero_or_same_number():
+    yakuso_solver = YakusoSolver()
+    rows = 5
+    cols = 6
+    template = get_template()
+    sum_cols = get_sum_columns()
+    solved = yakuso_solver.solve(template, sum_cols, rows, cols)[0]
+    for i in range(rows):
+        max_num = max(solved[i])
+        for j in range(cols):
+            assert solved[i][j] == max_num or solved[i][j] == 0
--- yakuso/yakuso_solver.py
+++ yakuso/yakuso_solver.py
@@ -2,40 +2,100 @@ from typing import List
 from ortools.sat.python import cp_model
 from itertools import product
 
+class SolutionAggregator(cp_model.CpSolverSolutionCallback):
+    def __init__(self, board_bools, board_numbers, rows, cols, sum_cols):
+        cp_model.CpSolverSolutionCallback.__init__(self)
+        self.board_bools = board_bools
+        self.board_numbers = board_numbers
+        self.rows = rows
+        self.cols = cols
+        self.sum_cols = sum_cols
+        self.solutions = []
+        
+    def on_solution_callback(self):
+        board = []
+        self.solutions.append(board)
+        for i in range(self.rows):
+            r = []
+            board.append(r)
+            for j in range(self.cols):
+                r.append(self.Value(self.board_bools[i][j] * self.Value(self.board_numbers[i])))
+        return board
 
 class YakusoSolver:
+
+    def __create_board_bools(self, model, rows, cols):
+        return [
+            [model.NewBoolVar(f"({_i},{_j})") for _j in range(cols)]
+            for _i in range(rows)
+        ]
+
+    def __create_board_maxima(self, model, rows, cols):
+        return [
+            model.NewIntVar(1, rows, f"row_({_i})")
+            for _i in range(rows)
+        ]
+
+    def __create_board_sums(self, model, rows, cols):
+        return [
+            model.NewIntVar(0, rows * rows, f"sum_col=({j})") for j in range(cols)
+        ]
+
+    def __add_sum_constraints(self, model, sums, sum_cols, cols):
+        for j in range(cols):
+            if len(sums[j]) > 0:
+                model.Add(int(sums[j]) == sum_cols[j])
+            else:
+                model.Add(sum_cols[j] == 0)
+
+    def __initialize_board_values(self, model, template, rows, cols, board_bools, board_maxima):
+        for i in range(rows):
+            for j in range(cols):
+                if len(template[i][j]) > 0:
+                    template_num = int(template[i][j])
+                    model.Add(
+                        board_bools[i][j]*template_num == template_num
+                    )
+                    if template_num > 0:
+                        model.Add(
+                            board_maxima[i] == template_num
+                        )
+
+    def __add_row_value_constraints(self, model, rows, cols, board_bools, board_maxima):
+        for i in range(rows):
+            total = 0
+            for j in range(cols):
+                total += board_bools[i][j]
+            model.Add(total == board_maxima[i])
+
+    def __add_different_maxima_constraint(self, model, board_maxima):
+        model.AddAllDifferent(board_maxima)
+
+    def __add_sum_per_column_constraint(self, model, sums, sum_cols, rows, board_bools, board_maxima):
+        for j in range(len(sums)):
+            if len(sums[j]) > 0:
+                cur_sum = sum_cols[j]
+                total = 0
+                for i in range(rows):
+                    mult = model.NewIntVar(0, rows, f"temp-mult-{i}")
+                    model.AddMultiplicationEquality(mult, [board_maxima[i], board_bools[i][j]])
+                    total += mult
+                model.Add(total == cur_sum)
+
     def solve(
         self, template: List[List[str]], sums: List[str], rows: int, cols: int
     ) -> List[List[int]]:
         model = cp_model.CpModel()
-        board = [
-            [model.NewIntVar(0, rows, f"({_i},{_j})") for _j in range(cols)]
-            for _i in range(rows)
-        ]
-        for i in range(rows):
-            for j in range(cols):
-                if len(template[i][j]) > 0:  # elements are strings, so check for length
-                    model.Add(
-                        board[i][j] == int(template[i][j])
-                    )  # and cast back to int
-        # Sum over all rows for column is equal to predefined value
-        for j in range(len(sums)):
-            if len(sums[j]) > 0:
-                total = 0
-                for i in range(rows):
-                    total += board[i][j]
-                model.Add(total == int(sums[j]))
+        board_bools = self.__create_board_bools(model, rows, cols)
+        board_maxima = self.__create_board_maxima(model, rows, cols)
+        sum_cols = self.__create_board_sums(model, rows, cols)
+        self.__add_sum_constraints(model, sums, sum_cols, cols)
+        self.__initialize_board_values(model, template, rows, cols, board_bools, board_maxima)
+        self.__add_row_value_constraints(model, rows, cols, board_bools, board_maxima)
+        self.__add_different_maxima_constraint(model, board_maxima)
+        self.__add_sum_per_column_constraint(model, sums, sum_cols, rows, board_bools, board_maxima)
         solver = cp_model.CpSolver()
-        status = solver.Solve(model)
-        if status == cp_model.FEASIBLE or status == cp_model.OPTIMAL:
-            return self.to_board(board, solver, rows, cols)
-        return []
-
-    def to_board(self, variables, solver, rows, cols):
-        board = []
-        for i in range(rows):
-            r = []
-            board.append(r)
-            for j in range(cols):
-                r.append(solver.Value(variables[i][j]))
-        return board
+        aggregator = SolutionAggregator(board_bools, board_maxima, rows, cols, sum_cols)
+        solver.parameters.enumerate_all_solutions = True
+        _ = solver.Solve(model, aggregator)
+        return aggregator.solutions
