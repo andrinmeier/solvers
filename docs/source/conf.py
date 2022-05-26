a68057440c4c70357bd65aa9bbe6c3fb2fcf2d02
Andrin Meier <andrin@pricemeier.ch>
2022-05-26 21:05:46 +0200

    Add auto summary

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,10 +30,15 @@ release = "1.0.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary"
+]
+
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,20 +1,8 @@
-.. ortools-puzzle-solvers documentation master file, created by
-   sphinx-quickstart on Thu May 26 20:17:45 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to ortools-puzzle-solvers's documentation!
 ==================================================
 
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+.. autosummary::
+   :toctree: _autosummary
+   :recursive:
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   solvers
