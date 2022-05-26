2c7bdf3c77eb9fba251be35786aae10b4b5c3ae3
Andrin Meier <andrin@pricemeier.ch>
2022-05-26 19:56:38 +0200

    Update metadata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ortools-puzzle-solvers'
+copyright = '2022, Andrin Meier'
+author = 'Andrin Meier'
 
 release = '0.1'
 version = '0.1.0'
